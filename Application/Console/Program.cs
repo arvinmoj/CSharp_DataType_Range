9f6f7777bac64cc934ddcec48fb80d9f5d4bc29b
Arvinmoj <arvinmojaverian@hotmail.com>
2022-07-29 13:34:21 +0430

    C# Data Type Range

---
+++ Application/Console/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Console
+{
+    internal static class Program : Object
+    {
+        static Program()
+        {
+        }
+
+        private static void Main()
+        {
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (sbyte)
+            System.Console.WriteLine(value: $"sbyte {nameof(sbyte.MinValue)} : {sbyte.MinValue:#,##0}");
+            // Max Value (sbyte)
+            System.Console.WriteLine(value: $"sbyte {nameof(sbyte.MaxValue)} : {sbyte.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (byte)
+            System.Console.WriteLine(value: $"byte {nameof(byte.MinValue)} : {byte.MinValue:#,##0}");
+            // Max Value (byte)
+            System.Console.WriteLine(value: $"byte {nameof(byte.MaxValue)} : {byte.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (short)
+            System.Console.WriteLine(value: $"short {nameof(short.MinValue)} : {short.MinValue:#,##0}");
+            // Max Value (short)
+            System.Console.WriteLine(value: $"short {nameof(short.MaxValue)} : {short.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (ushort)
+            System.Console.WriteLine(value: $"ushort {nameof(ushort.MinValue)} : {ushort.MinValue:#,##0}");
+            // Max Value (ushort)
+            System.Console.WriteLine(value: $"ushort {nameof(ushort.MaxValue)} : {ushort.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (int)
+            System.Console.WriteLine(value: $"int {nameof(int.MinValue)} : {int.MinValue:#,##0}");
+            // Max Value (int)
+            System.Console.WriteLine(value: $"int {nameof(int.MaxValue)} : {int.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+            // Min Value (uint)
+            System.Console.WriteLine(value: $"uint {nameof(uint.MinValue)} : {uint.MinValue:#,##0}");
+            // Max Value (uint)
+            System.Console.WriteLine(value: $"uint {nameof(uint.MaxValue)} : {uint.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (long)
+            System.Console.WriteLine(value: $"long {nameof(long.MinValue)} : {long.MinValue:#,##0}");
+            // Max Value (long)
+            System.Console.WriteLine(value: $"long {nameof(long.MaxValue)} : {long.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (ulong)
+            System.Console.WriteLine(value: $"ulong {nameof(ulong.MinValue)} : {ulong.MinValue:#,##0}");
+            // Max Value (ulong)
+            System.Console.WriteLine(value: $"ulong {nameof(ulong.MaxValue)} : {ulong.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (nint)
+            System.Console.WriteLine(value: $"nint {nameof(nint.MinValue)} : {nint.MinValue:#,##0}");
+            // Max Value (nint)
+            System.Console.WriteLine(value: $"nint {nameof(nint.MaxValue)} : {nint.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (nuint)
+            System.Console.WriteLine(value: $"nuint {nameof(nuint.MinValue)} : {nuint.MinValue:#,##0}");
+            // Max Value (nuint)
+            System.Console.WriteLine(value: $"nuint {nameof(nuint.MaxValue)} : {nuint.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+
+            System.Console.WriteLine("************************");
+
+            // Min Value (double)
+            System.Console.WriteLine(value: $"double {nameof(double.MinValue)} : {double.MinValue:#,##0}");
+            // Max Value (double)
+            System.Console.WriteLine(value: $"double {nameof(double.MaxValue)} : {double.MaxValue:#,##0}");
+
+            System.Console.WriteLine("************************");
+
+        }
+    }
+}
